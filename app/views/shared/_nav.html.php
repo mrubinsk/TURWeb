5403474619c50ce86d8ff40b21795563ed3d83b9
Michael J Rubinsky <mrubinsk@horde.org>
2011-01-29 21:46:02 -0500

    comment out navbar, remove old Horde_Controller styled ContentController.

--- app/views/shared/_nav.html.php
+++ app/views/shared/_nav.html.php
@@ -1,7 +1,7 @@
-  <!-- Main Navigation -->
+  <!-- Main Navigation 
   <div class="navigation">
     <a href="<?php echo $this->urlWriter->urlFor('home')?>">Home</a>
     <a href ="<?php echo $this->urlWriter->urlFor('software')?>">Software</a>
     <a href="<?php echo $this->urlWriter->urlFor('about')?>">About</a>
     <div class="clearer"><span></span></div>
-  </div>
+  </div>-->
