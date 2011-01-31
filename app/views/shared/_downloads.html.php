bd51d84645360b3fcafa12f5bce39a20f960fe56
Michael J Rubinsky <mrubinsk@horde.org>
2011-01-31 17:09:01 -0500

    hm the github badge interferes with the syntax highlighter. Disable it for now.

--- app/views/shared/_downloads.html.php
+++ app/views/shared/_downloads.html.php
@@ -1,13 +1,3 @@
 <div class="sidebar">
 <h1>Downloads</h1>
 </div>
-<div id="github-badge"></div>
-<script type="text/javascript" charset="utf-8">
-  GITHUB_USERNAME = "mrubinsk";
-  GITHUB_LIST_LENGTH = 10;
-  GITHUB_HEAD = "div"; // e.g. change to "h2" for wordpress sidebars
-  GITHUB_THEME = "white"; // try 'black'
-  GITHUB_TITLE = "github.org"
-  GITHUB_SHOW_ALL = "Show all"
-</script>
-<script src="http://drnicjavascript.rubyforge.org/github_badge/dist/github-badge-launcher.js" type="text/javascript"></script>
