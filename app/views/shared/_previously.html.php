11cd49793afab5e4ae00807b860bc573fde6b412
Michael J Rubinsky <mrubinsk@horde.org>
2011-09-04 15:29:00 -0400

    remove old links, add a recent images block from my Ansel install

--- app/views/shared/_previously.html.php
+++ app/views/shared/_previously.html.php
@@ -1,18 +1,9 @@
 <div id="previously" class="sidebar">
 <h1>Previously</h1>
-<?php echo $this->renderPartial('news', array('locals' => array('page' => $this->page, 
+<?php echo $this->renderPartial('news', array('locals' => array('page' => $this->page,
                                                           'pageCount' => $this->pageCount,
                                                           'summary' => $this->summary)));
 ?>
-<h1>Links</h1>
-<ul>
- <li>
-  <div class="rfNewsHeader"><a href="http://horde.org" title="The Horde Project">The Horde Project</a></div>
-  <div class="rfNewsBody">An open source web application framework and collection of applications that I'm currently involved with.</div>
- </li>
- <li>
-  <div class="rfNewsHeader"><a href="http://rubinskyfamily.com" title="My family website">Rubinsky Family Website</a></div>
-  <div class="rfNewsBody">In the interests of adding a more personal touch, this is my family website, with personal news, photos, and other goodies!</div>
- </li>
-</ul>
+<h1>Pictures</h1>
+<script type="text/javascript" src="http://h4.theupstairsroom.com/horde/services/imple.php?imple=Embed%2FimpleApp%3Dansel%2Fcount%3D10%2Fuser%3Dmike%2Fthumbsize%3Dmini%2Fthumbtype%3DSquareThumb%2Fcontainer%3D4e63c5bd0d76f%2F"></script><div id="4e63c5bd0d76f"></div>
 </div>
