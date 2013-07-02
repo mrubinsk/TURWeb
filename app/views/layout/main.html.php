52753d89e60db69f79407e2d494f2ff8e2ca1519
Michael J Rubinsky <mrubinsk@horde.org>
2013-07-01 21:18:37 -0400

    Update

--- app/views/layout/main.html.php
+++ app/views/layout/main.html.php
@@ -42,6 +42,20 @@
         return false;
     }
 </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-23347823-1']);
+  _gaq.push(['_setDomainName', '.theupstairsroom.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
  <div class="maincontainer">
@@ -54,17 +68,17 @@
 
      <!-- Right Hand Bar -->
      <div class="sidenav">
-      <?php echo $this->renderPartial('poweredby'); ?>
-      <?php echo $this->renderPartial('twitter');?>
-      <?php echo $this->renderPartial('cloud'); ?>
+      <?php echo $this->renderPartial('poweredby') ?>
+      <?php echo $this->renderPartial('twitter') ?>
+      <?php echo $this->renderPartial('wishlist') ?>      
+<?php echo $this->renderPartial('cloud'); ?>
       <?php echo $this->renderPartial(
         'previously',
         array(
           'locals' => array('page' => $this->page,
           'pageCount' => $this->pageCount,
-          'summary' => $this->summary)));?>
-      <?php echo $this->renderPartial('wishlist'); ?>
+          'summary' => $this->summary))) ?>
      </div>
      <div class="clearer"></div>
    </div>
- <?php echo $this->renderPartial('footer.html.php');?>
+ <?php echo $this->renderPartial('footer.html.php') ?>
